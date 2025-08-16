a112f8ab7ae2a309e0d286bf9d440262ae949f48
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-08-16 16:36:20 +0900

    first commit

---
+++ CsharpStudy.Asset/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpStudy.Asset;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CsharpStudy.Game/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpStudy.Game;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
