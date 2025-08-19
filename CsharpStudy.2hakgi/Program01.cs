9b44a6ef2da0ea28323de30305b97ce42350442e
sezondo <psdd205050@gmail.com>
2025-08-19 15:41:51 +0900

    AI 피드백에 따른 코드 수정

--- CsharpStudy.2hakgi/Program01.cs
+++ CsharpStudy.2hakgi/Program01.cs
@@ -8,12 +8,15 @@ class Program01
     {
     }
     
-    public string word = "";
+    public string word;
     public bool isVowel(int i)
     {
-        if (word[i] == 'a' || word[i] == 'A' || word[i] == 'e' || word[i] == 'E' || word[i] == 'i' || word[i] == 'I' || word[i] == 'o' || word[i] == 'O' || word[i] == 'u' || word[i] == 'U')
+        if (i <= word.Length)
         {
-            return true;
+            if (word[i] == 'a' || word[i] == 'A' || word[i] == 'e' || word[i] == 'E' || word[i] == 'i' || word[i] == 'I' || word[i] == 'o' || word[i] == 'O' || word[i] == 'u' || word[i] == 'U')
+            {
+                return true;
+            }
         }
         return false;
     }
