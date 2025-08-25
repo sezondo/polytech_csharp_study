d110ef9a5f251562ba57fab51a98cd9ca3b8c89d
sezondo <psdd205050@gmail.com>
2025-08-25 14:26:10 +0900

    디버그 완료

--- CsharpStudy.Debug/Program.cs
+++ CsharpStudy.Debug/Program.cs
@@ -7,6 +7,6 @@ class Program
         var controller = new YukymController();
 
         Console.WriteLine($"GetTyA() 결과: {controller.GetTyA()}");       // 8월 : 경오4국
-        Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}");       // 10시 : 갑자5국 
+        Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}");       // 10시 : 갑자5국  14시 : 갑자 7국
     }
 }
