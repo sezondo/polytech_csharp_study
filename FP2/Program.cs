a6338eae8057bc06081e5f7736b4071ca65cecac
sezondo <psdd205050@gmail.com>
2025-08-26 13:11:46 +0900

    문제 풀기전 커밋

---
+++ FP2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FP2;
+
+class Program
+{
+    public class Trader
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public Trader(string name, string city)
+        {
+            Name = name;
+            City = city;
+        }
+    }
+
+    public class Transaction
+    {
+        public Trader Trader { get; set; }
+        public int Year { get; set; }
+        public int Value { get; set; }
+
+        public Transaction(Trader trader, int year, int value)
+        {
+            Trader = trader;
+            Year = year;
+            Value = value;
+        }
+    }
+
+    public class MainClass
+    {
+        public static List<Transaction> transactions = new List<Transaction>
+        {
+            new Transaction(new Trader("Brian", "Cambridge"), 2011, 300),
+            new Transaction(new Trader("Raoul", "Cambridge"), 2012, 1000),
+            new Transaction(new Trader("Raoul", "Cambridge"), 2011, 400),
+            new Transaction(new Trader("Mario", "Milan"), 2012, 710),
+            new Transaction(new Trader("Mario", "Milan"), 2012, 700),
+            new Transaction(new Trader("Alan", "Cambridge"), 2012, 950)
+        };
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+}
