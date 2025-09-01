eb0e786a73907c95454456bfec36de6604773446
sezondo <psdd205050@gmail.com>
2025-09-01 13:13:20 +0900

    예외의 대한 답

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        var numString = "10.5";
+        int num;
         
+        try
+        {
+            num = int.Parse(numString);
+        }
+        catch (System.Exception e)
+        {
+            num = 0;
+        }
+        
+        Console.WriteLine(num);
     }
+
 }
